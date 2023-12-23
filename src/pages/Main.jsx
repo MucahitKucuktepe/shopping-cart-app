e0ecad7331163cb227bae092778ca2777982db16
MucahitKucuktepe <mehmet20132204@gmail.com>
2023-12-23 22:52:26 +0300

    main component buttons added

--- src/pages/Main.jsx
+++ src/pages/Main.jsx
@@ -1,9 +1,29 @@
-import React from 'react'
+import React from "react";
+import { useNavigate } from "react-router-dom";
 
 const Main = () => {
+  const navigate = useNavigate();
   return (
-    <div>Main</div>
-  )
-}
+    <main className="main">
+      <div>
+        <h1>Welcome to Shopping Cart Aplication</h1>
+        <div className="main-buttons">
+          <button
+            className="btn btn-outline-primary px-4"
+            onClick={() => navigate("/new-product")}
+          >
+            Add New Product
+          </button>
+          <button
+            className="btn btn-primary ms-3 px-4"
+            onClick={() => navigate("/products")}
+          >
+            See Products
+          </button>
+        </div>
+      </div>
+    </main>
+  );
+};
 
-export default Main
+export default Main;
